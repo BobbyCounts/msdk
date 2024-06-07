36399d9bfff1a7bf9cb30abe3059ed0274773301
Bobby Counts <bobby.counts@analog.com>
2024-06-07 09:47:40 -0500

    Add LINKER_GEN_FLAGS description in the project.mk file

--- Examples/MAX32657/Hello_World_TZ/Secure/project.mk
+++ Examples/MAX32657/Hello_World_TZ/Secure/project.mk
@@ -18,3 +18,16 @@ MSECURITY_MODE=SECURE
 # Add path to Non-Secure project.
 SECURE_CODE_DIR=./
 NONSECURE_CODE_DIR=../NonSecure
+
+# Variable LINKER_GEN_FLAGS sets the options for the linker file generator
+#  --sram_exe            Run code in SRAM instead of flash
+#  --secure_flip         Put the secure section at the 2nd half of flash
+#                        instead of the 1st
+#  --nsc_size NSC_SIZE   Set the size of the non-secure callable section in kB.
+#                        Default=8kB
+#  --flash_size FLASH_SIZE
+#                        Set the size of the flash in kB. Default=1024kB
+#  --sram_size SRAM_SIZE
+#                        Set the size of the SRAM in kB. Default=256kB
+#  --print_result        Print linker calculation results to console
+LINKER_GEN_FLAGS= --flash_size 512 #Reducing flash size to run on emulator
